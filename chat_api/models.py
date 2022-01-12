ace7b47dc2b9e17481712cfc2c0a34ac5eb03b19
Duncan Watson <dwwatson777@me.com>
2022-01-12 16:16:15 -0500

    duncan front end stuff

--- chat_api/models.py
+++ chat_api/models.py
@@ -1,3 +1,50 @@
+import uuid
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+from django.contrib.auth.models import User
+from 
 
-# Create your models here.
+def validate_message_content(content):
+    if content is None or content == "" or content.isspace():
+        raise ValidationError(
+            'Content is empty/invalid',
+            code='invalid',
+            params={'content': content},
+        )
+
+
+class Message(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        null=False,
+        default=uuid.uuid4,
+        editable=False
+    )
+    room = models.ForeignKey(
+        
+    )
+    
+    author = models.ForeignKey(
+        User,
+        blank=False,
+        null=False,
+        related_name='author_messages',
+        on_delete=models.CASCADE
+    )
+    content = models.TextField(validators=[validate_message_content])
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def last_50_messages():
+        return Message.objects.order_by('-created_at').all()[:50]
+
+class UserMessages(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    # last_read_date = models.DateTimeField(
+    #     auto_now_add=True,
+    #     blank=False,
+    #     null=False
+    # )
+    # def __str__(self):
+    #     return self.User
