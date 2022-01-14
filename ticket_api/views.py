eb532cb78ee465183c60a460e3b311406c14affe
MIchael Robertson <mwrobertson85@gmail.com>
2022-01-14 01:40:08 -0600

    Some edits to Ticket_API to filter based on user_ID

--- ticket_api/views.py
+++ ticket_api/views.py
@@ -6,4 +6,15 @@ class TicketViewSet(viewsets.ModelViewSet):
     serializer_class = TicketSerializer
 
     def get_queryset(self):
-        return Ticket.objects.all()
+        queryset = Ticket.objects.all()
+        aid_provider_id = self.request.query_params.get('aid_provider_id')
+        aid_recipient_id = self.request.query_params.get('aid_recipient_id')
+        sponsor_id = self.request.query_params.get('sponsor_id')
+        if aid_provider_id is not None:
+            queryset = queryset.filter(aid_provider_id=aid_provider_id)
+        if aid_recipient_id is not None:
+            queryset = queryset.filter(aid_recipient_id=aid_recipient_id)
+        if sponsor_id is not None:
+            queryset = queryset.filter(sponsor_id=sponsor_id)
+        
+        return queryset
