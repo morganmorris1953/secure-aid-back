9a2fec12e8d35ea814648cc0071b06b7792692d5
MIchael Robertson <mwrobertson85@gmail.com>
2022-01-16 22:00:26 -0600

    Small change to RecipientOutput

--- user_api/api.py
+++ user_api/api.py
@@ -2,6 +2,7 @@ from ninja import Router, Schema
 from typing import List
 from django.shortcuts import get_list_or_404
 from . import models
+from django.contrib.auth.models import User
 
 
 router = Router()
@@ -9,11 +10,14 @@ router = Router()
 
 class RecipientOutput(Schema):
     id: int
-    sponsor_id: int
-    recipient_id: int
+    username: str
+    first_name: str
+    last_name: str
 
 
 @router.get("recipients", url_name="list_recipients", response=List[RecipientOutput])
 def list_recipients(request, sponsor_id: int):
-    recipients_list = get_list_or_404(models.Referral, sponsor_id=sponsor_id)
+    # recipients_list = get_list_or_404(models.Referral, sponsor_id=sponsor_id)
+    recip_ids = models.Referral.objects.filter(sponsor_id=sponsor_id).values_list("recipient_id")
+    recipients_list = models.User.objects.filter(pk__in=recip_ids)
     return recipients_list
